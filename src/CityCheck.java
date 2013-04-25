b836229c8c301b8ad1ad63cc0b79e5e18945f2aa
Wehttam664 <Matt@MayateckInnovations.net>
2013-04-25 11:18:57 -0700

    Added skeleton of the fetch/check system.

--- src/CityCheck.java
+++ src/CityCheck.java
@@ -1,1 +1,40 @@
-# TMP
+package net.mayateck;
+
+import org.Bukkit.Player.*;
+
+@SupressWarnings("unused")
+public class CityCheck {
+	
+	public static boolean chkForExistantCity(String name){
+		
+		return true;
+	}
+	
+	public static boolean chkForExistantClaim(String name, double xPos, double zPos){
+		
+		return true;
+	}
+	
+	public static boolean chkForEcon(Player plyr, int cost){
+		
+		return true;
+	}
+	
+	public static String fetchCityFromPlayer(Player plyr){
+		String city;
+		
+		return city;
+	}
+	
+	public static Player fetchPlayerFromCity(String name){
+		Player plyr;
+		
+		return plyr
+	}
+	
+	public static String[] fetchWarpsFromCity(String name){
+		String[] warps;
+		
+		return warps;
+	}
+}
